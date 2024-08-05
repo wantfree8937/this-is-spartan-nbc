07bebf1e5f2043d26cfcdef6a4644e255d77d425
민광규 <167201080+JollyDude16@users.noreply.github.com>
2024-08-05 10:53:05 +0900

    ✨ update: 초기 세팅 2 : run dev, run migrate 추가, GAME_DB 제거

--- src/constants/env.js
+++ src/constants/env.js
@@ -11,9 +11,3 @@ export const DB1_USER = process.env.DB1_USER || 'user1';
 export const DB1_PASSWORD = process.env.DB1_PASSWORD || 'password1';
 export const DB1_HOST = process.env.DB1_HOST || 'localhost';
 export const DB1_PORT = process.env.DB1_PORT || 3306;
-
-export const DB2_NAME = process.env.DB2_NAME || 'database2';
-export const DB2_USER = process.env.DB2_USER || 'user2';
-export const DB2_PASSWORD = process.env.DB2_PASSWORD || 'password2';
-export const DB2_HOST = process.env.DB2_HOST || 'localhost';
-export const DB2_PORT = process.env.DB2_PORT || 3306;
