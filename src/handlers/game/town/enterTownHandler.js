6794fbc066d336acc5e571cebcd4b014b745d7c7
민광규 <167201080+JollyDude16@users.noreply.github.com>
2024-08-06 12:49:49 +0900

    ✨ update: 오류사항 공유

--- src/handlers/game/town/enterTownHandler.js
+++ src/handlers/game/town/enterTownHandler.js
@@ -1,13 +1,58 @@
+import { loadGameAssets } from '../../../init/assets.js';
 import { getUserBySocket } from '../../../session/user.session.js';
 import { createResponse } from '../../../utils/response/createResponse.js';
+import { v4 as uuidv4 } from 'uuid';
 
+const gameAssets = await loadGameAssets();
+
+const getClassStats = (classId) => {
+  for (let stat of gameAssets.classStat.data) {
+    if (stat.class === classId) {
+      return stat;
+    }
+  }
+  return null; //에러?
+};
 const enterTownHandler = async ({ socket, payload }) => {
   const { nickname } = payload;
-  const classCategory = payload.class;
-  const playerInfo = {};
-  const enterTownResponse = createResponse('responseTown', 'S_Enter', {
-    player: playerInfo,
-  });
+  const classId = payload.class;
+
+  const playerId = uuidv4(); //<= 요걸 어디 저장해놓고 특정할 수 있어야할텐데..userSession? userClass?
+
+  //classCategory에 맞는 클래스 데이터를 class.json에서 불러오기
+  const classStats = getClassStats(classId);
+
+  const statInfo = {
+    level: 1,
+    hp: classStats.maxHp,
+    maxHp: classStats.maxHp,
+    mp: classStats.maxMp,
+    maxMp: classStats.maxMp,
+    atk: classStats.atk,
+    def: classStats.def,
+    magic: classStats.magic,
+    speed: classStats.speed,
+  };
+
+  const transformInfo = {
+    posX: 0,
+    posY: 1,
+    posZ: 0,
+    rot: 0,
+  };
+
+  const player = {
+    playerId: playerId,
+    nickname: nickname,
+    class: classStats.class,
+    transformInfo: transformInfo,
+    statInfo: statInfo,
+  };
+
+  console.log(player);
+  const enterTownResponse = createResponse('responseTown', 'S_Enter', player);
+
+  socket.write(enterTownResponse);
 };
 
 export default enterTownHandler;
