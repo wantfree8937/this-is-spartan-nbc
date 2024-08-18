30b997142a32317dc7cb1f4c79fcda6ea8396b8f
uchanjeon <uchanjeon@gmail.com>
2024-08-18 16:28:46 +0900

    game.db.js-game.queries.js-game_db.sql 작성완료

--- src/db/game/game.db.js
+++ src/db/game/game.db.js
@@ -1,6 +1,27 @@
-export const getGameClassStats = async () => {
-    const [rows] = await pools.GAME_DB.query(GAME_QUERIES.FIND_ALL_CLASS_STATS);
-    const camelCasedRows = rows.map(toCamelCase);
-    const jsonString = JSON.stringify(camelCasedRows);
-    return jsonString;
+export const getUserStats = async () => {
+  const [rows] = await pools.GAME_DB.query(GAME_QUERIES.FIND_ALL_USER_STATS);
+  const camelCasedRows = rows.map(toCamelCase);
+  const jsonString = JSON.stringify(camelCasedRows);
+  return jsonString;
+};
+
+export const getMonsterStats = async () => {
+  const [rows] = await pools.GAME_DB.query(GAME_QUERIES.FIND_ALL_MONSTER_STATS);
+  const camelCasedRows = rows.map(toCamelCase);
+  const jsonString = JSON.stringify(camelCasedRows);
+  return jsonString;
+};
+
+export const getMapCodes = async () => {
+  const [rows] = await pools.GAME_DB.query(GAME_QUERIES.FIND_ALL_MAP_CODES);
+  const camelCasedRows = rows.map(toCamelCase);
+  const jsonString = JSON.stringify(camelCasedRows);
+  return jsonString;
+};
+
+export const getTextInfo = async () => {
+  const [rows] = await pools.GAME_DB.query(GAME_QUERIES.FIND_ALL_TEXT_INFO);
+  const camelCasedRows = rows.map(toCamelCase);
+  const jsonString = JSON.stringify(camelCasedRows);
+  return jsonString;
 };
